dc38df66fa94e36afca85991f3602099386724c0
Rabram01 <raph.brambilla@gmail.com>
2017-08-16 11:19:32 +0200

    devTesting
    testDev

---
+++ src/pages/home/home.ts
@@ -0,0 +1,38 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+
+  }
+
+   ionViewDidLoad() { // Added this function to loadMap
+    this.loadMap();
+  }
+
+   loadMap() {
+    this.geolocation.getCurrentPosition().then((position) => {
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+}
